af1fa8613768e82e16e9d9b7fda1d1aea50b99cf
Ho Jin Lee <65278309+ili0820@users.noreply.github.com>
2023-08-28 10:30:38 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 830 ms, Memory: 25,064 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,97 +1,85 @@
-
-import java.io.*;
-import java.util.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Solution {
-    static int T;
-    static int N,total_cnt,total_len;
-    static int[][] board;
-    static ArrayList<Point> cores;
-    static int[] dx= new int[] {0,0,-1,1};
-    static int[] dy= new int[] {-1,1,0,0};
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
-        for (int tc = 1; tc <= T; tc++) {
-            cores = new ArrayList<>();
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            board = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++)
-                    board[i][j] = Integer.parseInt(st.nextToken());
-            }
-            //가장자리 제외 코어들 저장
-            for (int i = 1; i < N; i++) {
-                for (int j = 1; j < N; j++) {
-                    if(board[i][j]==1)cores.add(new Point(i,j));
-                }
-            }
-            total_cnt=Integer.MIN_VALUE;
-            total_len=Integer.MAX_VALUE;
-            dfs(0,0,0);
-            sb.append('#').append(tc).append(' ').append(total_len).append('\n');
-        }
-        System.out.print(sb);
-    }
-    public static void dfs(int depth,int cnt, int len){
-        if(depth==cores.size()){
-                // System.out.println(cnt);
-            if(total_cnt<cnt){
-                total_cnt=cnt;
-                total_len=len;
-            }
-            else if(total_cnt==cnt){
-                if(total_len>len)total_len=len;
-            }
-            return;
-        }
-        int x=cores.get(depth).x;
-        int y=cores.get(depth).y;
-        int nx,ny;
-        for(int i=0;i<4;i++){
-            int wires=0;
-            boolean possible=false;
-            nx=x;
-            ny=y;
-            while(true){
-                nx+=dx[i];
-                ny+=dy[i];
-                if(!isin(nx,ny)){
-                    possible=true;
-                    break;
-                }
-                if(board[nx][ny]!=0)break;
-                wires+=1;
-            }
-            if(possible){
-                for(int j=1;j<=wires;j++){//와이어 설치
-                    board[x+dx[i]*j][y+dy[i]*j]=1;
-                }
-                dfs(depth+1,cnt+1,len+wires);
-                
-                for(int j=1;j<=wires;j++){//와이어 철거
-                    board[x+dx[i]*j][y+dy[i]*j]=0;
-                }
-            }
-            else dfs(depth+1,cnt,len);
-        }
-    }
-    public static class Point {
-        int x;
-        int y;
-
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    public static boolean isin(int x,int y){
-        return 0<=x&&x<N && 0<=y&&y<N;
-    }
+	static int t,N,c,answer;
+	static ArrayList<Point> cores;
+	static int [][] map;
+	static int dx[] = {0,0,-1,1} , dy[] = {-1,1,0,0};
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		t = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= t; test_case++) {
+			N= Integer.parseInt(br.readLine());
+			map = new int[N][N];
+			cores  = new ArrayList<Point>();
+			for (int i = 0; i < N; i++) {
+				String[] s = br.readLine().split(" ");
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(s[j]);
+					if(i !=0 && j != 0 && j!=N-1 && i!=N-1) {
+						if(map[i][j] == 1)cores.add(new Point(j,i));
+					}
+				}
+			}
+			c=0;
+			answer = 0;
+			find(0,0,0);
+			System.out.println("#" + test_case +" "+ answer);
+			
+		}
+		
+	}
+	private static void find(int cnt, int coreCnt , int sum) {
+		if(cnt == cores.size()) {
+			if(c < coreCnt) {
+				c = coreCnt;
+				answer = sum;
+			}
+			else if(c == coreCnt) {
+				answer = Math.min(answer, sum);
+			}
+			return;
+		}
+		Point p = cores.get(cnt);
+		for (int i = 0; i < 4; i++) {
+			int realx = p.x;
+			int realy = p.y;
+			int count =0;
+			int tempx = p.x;
+			int tempy = p.y;
+		
+			while(tempx >0 && tempx <N-1 && tempy>0 && tempy<N-1) {
+				tempx += dx[i];
+				tempy += dy[i];
+				if(map[tempy][tempx] != 0) {
+					count =0;
+					break;
+				}
+				count++;
+			}
+			if(count == 0 ) {
+				find(cnt+1,coreCnt,sum);
+			}
+			else {
+				for (int j = 0; j < count; j++) {
+					realx += dx[i];
+					realy += dy[i];
+					map[realy][realx] = 2;
+				}
+				find(cnt+1,coreCnt+1,sum+count);
+				 realx = p.x;
+				 realy = p.y;
+				for (int j = 0; j < count; j++) {
+					realx += dx[i];
+					realy += dy[i];
+					map[realy][realx] = 0;
+				}
+			}
+		}
+		
+	}
 }
