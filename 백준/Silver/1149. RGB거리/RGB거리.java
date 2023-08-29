47cd016e3a6785bcddca4fba8f3ac47edc43367c
Ho Jin Lee <65278309+ili0820@users.noreply.github.com>
2023-08-29 15:38:09 +0900

    [Silver I] Title: RGB거리, Time: 248 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,38 +1,44 @@
 
-import java.io.*;
-import java.util.*;
-public class  Main {
-    static int N;
-    static int[][] costs;
-    static int[][] dp;
-    public static void main(String [] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        
-        costs=new int[N][3];
-        
-        for(int i=0;i<N;i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=0;j<3;j++)
-            costs[i][j]=Integer.parseInt(st.nextToken());
-        }
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-        dp=new int[N][3];
-        //dp 는 i번째 집을 j 번째 색을 칠하는 경우
-        //costs는 i번째 집을 j 번째 색으로 칠하는경우
-        dp[0][0]=costs[0][0];
-        dp[0][1]=costs[0][1];
-        dp[0][2]=costs[0][2];
-        for(int i=1;i<N;i++){
-            dp[i][0]=Math.min(dp[i-1][1],dp[i-1][2])+costs[i][0];
-            dp[i][1]=Math.min(dp[i-1][0],dp[i-1][2])+costs[i][1];
-            dp[i][2]=Math.min(dp[i-1][1],dp[i-1][0])+costs[i][2];
+public class Main {
+	static int N, marking[],table[][],dp[][];
+	
+	public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        table = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            String[] s = br.readLine().split(" ");
+            int r = Integer.parseInt(s[0]);
+            int g = Integer.parseInt(s[1]);
+            int b = Integer.parseInt(s[2]);
+            table[i][0] = r;
+            table[i][1] = g;
+            table[i][2] = b;
         }
-        int min_value=Integer.MAX_VALUE;
-        for(int i=0;i<3;i++){
-            min_value=Math.min(min_value,dp[N-1][i]);
-        }
-        System.out.print(min_value);
-    }
+        dp = new int[N][3];
+
+
+        dp[0][0] = table[0][0]; // 첫째빨
+        dp[0][1] = table[0][1]; // 첫재 그
+        dp[0][2] = table[0][2]; // 첫째 파
+	
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < 3; j++) {
+				int value = Integer.MAX_VALUE;
+				for (int z = 0; z < 3; z++) {
+					if(j == z) continue;
+					if(value > dp[i-1][z]) {
+						value = dp[i-1][z];
+					}
+					dp[i][j] = value+table[i][j];
+				}
+			}
+		}
+		System.out.println(Arrays.stream(dp[N-1]).min().getAsInt());
+	}
+
 }
